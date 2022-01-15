eb9d48a69c37b0ec8764be377f2f28c4abd48505
hbheesetti <hbheeset@ualberta.ca>
2022-01-15 15:44:30 -0700

    adding prof.js and history.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route, Switch, Router } from "react-router-dom";
+import history from "./History";
+import prof from "./prof";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+	render() {
+		let renderComponent;
+		renderComponent = (
+			<Router history={history}>
+				<Switch>
+					<Route path="/prof" exact component={prof} />
+				</Switch>
+			</Router>
+		);
+		return <div>{renderComponent}</div>;
+	}
 }
 
 export default App;
