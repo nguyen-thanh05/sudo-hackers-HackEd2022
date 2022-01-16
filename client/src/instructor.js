3d3265ba78e3e4fdffe2a97f7738a98110003bf2
Leen-Alzebdeh <40804576+Leen-Alzebdeh@users.noreply.github.com>
2022-01-15 21:23:45 -0700

    add func

--- client/src/instructor.js
+++ client/src/instructor.js
@@ -1,18 +1,37 @@
 import React, { Component } from "react";
-import { Button } from "rsuite";
+import { Button, Container } from "rsuite";
+import { count } from "rsuite/esm/utils/ReactChildren";
 
 class instructor extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			hello: "hello ",
+			headerStr: "Class of \n Date: \n ",
+			instrStr: "Swipe to discard question.\n",
+			greenColor: "#120078"
+			//TODO: popup letting you know that you will get a notification
 		};
+		this.QColor = this.QColor();
 	}
 
+	QColor() {
+		//queue.total((err, count) => {
+		//	console.log('This queue has seen %d messages', count)
+		//});
+		if (count == 1) return "rgb(251, 236, 167)"
+		else return "rgb(246, 246, 246)"
+	}
 	render() {
 		return (
 			<div>
-				<Button>{this.state.hello}</Button>
+				<Container
+					style={{
+						backgroundColor: "rgb(39,94,37)",
+						opacity: 0.6,
+						padding: 10
+					}}>
+
+				</Container>
 			</div>
 		);
 	}
