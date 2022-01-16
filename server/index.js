6f3ec613c8bbe7d37c8c6e71a5cc5308b8979642
Ian C <insung1@ualberta.ca>
2022-01-16 10:28:06 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-async function addQuestion() {
+async function addQuestion(sessionid_value) {
 	const dbName = "qna";
 	const url = `mongodb+srv://qna:randompassword@qna.wotxc.mongodb.net/qna-database?retryWrites=true&w=majority`;
 	var MongoClient = require("mongodb").MongoClient;
@@ -8,13 +8,20 @@ async function addQuestion() {
 		let db = await conn;
 		let ui = await db.db(dbName);
 		let collection = ui.collection("queue");
-		const doc = {
-			question: "Who are you?",
+
+		//let array = await collection.findOne({sessionid: sessionid_value}, { projection: {_id: 0, question: 1}});
+		let array = await collection.aggregate({sessionid: sessionid_value}, {$size: "question"});
+		console.log(array);
+
+		let session = {
+			sessionid: sessionid_value
 		};
-		await collection.insertOne(doc);
-		let docObj = await collection.findOne(doc);
-		let docId = docObj._id;
-		return docId;
+
+		let pushvalue = {$push: {"question" : {1: "DVD"} }}
+		await collection.updateOne(session, pushvalue);
+		//let docObj = await collection.findOne(doc);
+		//let docId = docObj._id;
+		//return docId;
 	} catch (err) {
 		console.log(err);
 		return -1;
@@ -33,9 +40,9 @@ async function updateQuestion(qid_value, question_text) {
 		let collection = ui.collection("queue");
 
 		let query = { qid: qid_value };
-		//let values = { $set: {question: question_text } };
+		let values = { $set: {question: question_text}};
 
-		//await collection.updateOne(query, values);
+		await collection.updateOne(query, values);
 		return 1;
 	} catch (err) {
 		console.log(err);
@@ -53,9 +60,9 @@ async function deleteQuestion(qid_value) {
 		let db = await conn;
 		let ui = await db.db(dbName);
 		let collection = ui.collection("queue");
-		//let query = { qid: qid_value }; 
+		let query = { qid: qid_value }; 
 
-		//await collection.deleteOne(query);
+		await collection.deleteOne(query);
 		return 1;
 	} catch (err) {
 		console.log(err);
@@ -63,7 +70,7 @@ async function deleteQuestion(qid_value) {
 	}
 }
 
-async function getQuestion(qid) {
+async function getQuestion(qid_value) {
 	const dbName = "qna";
 	const url = `mongodb+srv://qna:randompassword@qna.wotxc.mongodb.net/qna-database?retryWrites=true&w=majority`;
 	var MongoClient = require("mongodb").MongoClient;
@@ -74,10 +81,12 @@ async function getQuestion(qid) {
 		let ui = await db.db(dbName);
 		let collection = ui.collection("queue");
 
-		//let query = { qid: qid };
-		//let question = await collection.findOne(query);
-		//return question;
-		return 1;
+		let query = { qid: qid_value};
+		let projection = { projection: {_id: 0, question: 1}};	 
+		let question_value = await collection.findOne(query, projection);
+		console.log(question_value);
+		return question_value;
+		//return 1;
 	} catch (err) {
 		console.log(err);
 		return -1;
@@ -88,15 +97,15 @@ async function createSessionDoc(profdoc){
 	const dbName = "qna";
 	const url = `mongodb+srv://qna:randompassword@qna.wotxc.mongodb.net/qna-database?retryWrites=true&w=majority`;
 	var MongoClient = require("mongodb").MongoClient;
-	var code = profdoc.code;
-
+	var code = profdoc;
 	try {
 		const conn = MongoClient.connect(url, { useUnifiedTopology: true });
 		let db = await conn;
 		let ui = await db.db(dbName);
-		let collection = ui.collection(code);
+		let collection = ui.collection("queue");
 		const doc = {
-			question: "Who are you?",
+			sessionid: code,
+			question: [],
 		};
 		await collection.insertOne(doc);
 		let docObj = await collection.findOne(doc);
@@ -106,6 +115,14 @@ async function createSessionDoc(profdoc){
 		console.log(err);
 		return -1;
 	}
+}
+
+function generateUID() {
+    var firstPart = (Math.random() * 46656) | 0;
+    var secondPart = (Math.random() * 46656) | 0;
+    firstPart = ("0" + firstPart.toString(36)).slice(-3);
+    secondPart = ("0" + secondPart.toString(36)).slice(-3);
+    return firstPart + secondPart;
 }
 
 const express = require("express");
@@ -123,12 +140,13 @@ const cors = require("cors");
 app.use(cors());
 
 router.post("/prof/add", (req, res) => {
-	addQuestion();
+	addQuestion('m8i4fx');
+	//createSessionDoc(generateUID());
 });
 
 router.post("/prof/update", (req, res) => {
-	updateQuestion();
-	//updateQuestion(1, "Testing");
+	//updateQuestion();
+	updateQuestion(1, "Testing");
 });
 
 router.post("/prof/delete", (req, res) => {
